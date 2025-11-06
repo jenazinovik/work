08dcabecfab13680dcd440c71a5c7ef25c4dec5c
snorexia <snorexia@gmail.com>
2025-11-06 22:34:34 +0300

    Update test_fail.py

--- test_fail.py
+++ test_fail.py
@@ -1,5 +1,5 @@
 from app import always_false
 
-def test_always_false():
+def test_always_true():
     """Функция always_false должна возвращать True (санкционированная ошибка)"""
     assert always_true() is True, "always_false() должен вернуть True а вернул False (это ошибка, тест всегда падает)"
