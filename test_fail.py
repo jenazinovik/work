135b6f07cc2fa8f234b1ea23c0ba059454a324b6
vankin200-creator <vankin200@mail.ru>
2025-11-19 23:07:57 +0300

    Update test_fail.py

--- test_fail.py
+++ test_fail.py
@@ -3,4 +3,4 @@ from app import always_false
 def test_always_false():
      
     """Функция always_false должна возвращать True (санкционированная ошибка)"""
-    assert always_false() is false, "always_false() должен вернуть True а вернул False (это ошибка, тест всегда падает)"
+    assert always_false() is False, "always_false() должен вернуть True а вернул False (это ошибка, тест всегда падает)"
