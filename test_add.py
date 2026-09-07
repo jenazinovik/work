434f84d64fe4baa849b03b76d95766f63516ca58
sinokotca-qa <sinokotca@gmail.com>
2026-09-07 12:34:10 +0300

    Update test_add.py

--- test_add.py
+++ test_add.py
@@ -2,8 +2,8 @@ from app import add
 
 def test_add_positive_numbers():
     """Проверяем сложение двух положительных чисел"""
-    assert add(2, -3) == 5, "add(2, 3) должен вернуть 5"
+    assert add(2, 3) == 5, "add(2, 3) должен вернуть 5"
 
 def test_add_negative_numbers():
     """Проверяем сложение отрицательных чисел"""
-    assert add(-2, -3) == -5, f"add сумма (-2, -3) должен вернуть -5"
+    assert add(-2, 3) == -5, f"add сумма (-2, -3) должен вернуть -5"
